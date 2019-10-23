65e599fc06c957038f255e87a6b3de569a426653
Chris Waring <chris@wwaves.co>
2019-10-23 17:23:33 +0100

    move head meta and deny indexing

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,58 +1,58 @@
 // .vuepress/config.js
 module.exports = {
+  head: [
+    ['meta', { robots: 'noindex' }], // deny search indexing for now
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      }
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      }
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      }
+    ],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#3a0839'
+      }
+    ],
+    ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#3a0839' }],
+    [
+      'meta',
+      {
+        name: 'msapplication-config',
+        content: '/browserconfig.xml'
+      }
+    ],
+    ['meta', { name: 'theme-color', content: '#5bbad5' }]
+  ],
   locales: {
     '/': {
       lang: 'en-US',
       title: 'IPFS Docs',
-      description: 'IPFS Documentation',
-      head: [
-        ['meta', { robots: 'NOINDEX, NOFOLLOW' }],
-        [
-          'link',
-          {
-            rel: 'apple-touch-icon',
-            sizes: '180x180',
-            href: '/apple-touch-icon.png'
-          }
-        ],
-        [
-          'link',
-          {
-            rel: 'icon',
-            type: 'image/png',
-            sizes: '32x32',
-            href: '/favicon-32x32.png'
-          }
-        ],
-        [
-          'link',
-          {
-            rel: 'icon',
-            type: 'image/png',
-            sizes: '16x16',
-            href: '/favicon-16x16.png'
-          }
-        ],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-        [
-          'link',
-          {
-            rel: 'mask-icon',
-            href: '/safari-pinned-tab.svg',
-            color: '#3a0839'
-          }
-        ],
-        ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#3a0839' }],
-        [
-          'meta',
-          {
-            name: 'msapplication-config',
-            content: '/browserconfig.xml'
-          }
-        ],
-        ['meta', { name: 'theme-color', content: '#5bbad5' }]
-      ]
+      description: 'IPFS Documentation'
     }
   },
   themeConfig: {
