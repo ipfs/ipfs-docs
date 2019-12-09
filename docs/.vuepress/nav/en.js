f8562eb02dd0dced9cede83b743be5098f228ad6
Eric Ronne <eric@protocol.ai>
2019-12-09 10:50:34 -0600

    link patch

--- docs/.vuepress/nav/en.js
+++ docs/.vuepress/nav/en.js
@@ -1,6 +1,7 @@
 module.exports = [
   {
     text: 'Suggest a Topic',
-    link: 'https://ipfs.canny.io/docs-features'
+    link:
+      'https://github.com/ipfs/docs/issues/new?assignees=&labels=OKR+3%3A+Content+Improvement%2C+docs-ipfs&template=content-request.md&title=%5BCONTENT+REQUEST%5D+%28add+your+title+here%21%29'
   }
 ]
