422da744bba28f2a39de7c3bdfa85dbfaedd800b
Chris Waring <chris@wwaves.co>
2019-12-10 21:45:12 +0000

    add content to LegacyCallout

--- docs/.vuepress/theme/components/LegacyCallout.vue
+++ docs/.vuepress/theme/components/LegacyCallout.vue
@@ -1,25 +1,43 @@
 <template>
-  <div class="tip custom-block legacy-callout">
-    <p>
-      This site is in beta.
-      <a
-        href="https://github.com/ipfs/docs/issues/new?assignees=&labels=OKR+3%3A+Content+Improvement%2C+docs-ipfs&template=documentation-issue.md&title=%5BDOCS+ISSUE%5D+Beta+Site+Feedback"
-        target="_blank"
-        >Submit feedback</a
-      >
-      <span v-if="$site.themeConfig.betaTestFormUrl">
-        or
-        <a :href="$site.themeConfig.betaTestFormUrl" target="_blank"
-          >become a tester</a
-        >
-      </span>
-      to help us improve it!
-      <span v-if="legacyUrl" class="content-status-status">
-        <br />
-        <a target="_blank" :href="legacyUrl">View this page on legacy site</a>
-      </span>
-    </p>
-  </div>
+  <main class="content-status">
+    <div>
+      <div class="tip custom-block legacy-callout">
+        <p>
+          <b>This site is in beta. Help us improve it!</b>
+        </p>
+        <p>
+          <a
+            href="https://github.com/ipfs/docs/issues/new?assignees=&labels=OKR+3%3A+Content+Improvement%2C+docs-ipfs&template=content-request.md&title=%5BCONTENT+REQUEST%5D+%28add+your+title+here%21%29"
+            target="_blank"
+            >Suggest new content</a
+          >
+          <br />
+          <a href="https://ipfs.canny.io/docs-features" target="_blank"
+            >Request features</a
+          >
+          <br />
+          <a
+            href="https://github.com/ipfs/docs/issues/new?assignees=&labels=OKR+3%3A+Content+Improvement%2C+docs-ipfs&template=documentation-issue.md&title=%5BDOCS+ISSUE%5D+%28add+your+title+here%21%29"
+            target="_blank"
+            >Give general feedback</a
+          >
+          <br />
+          <span v-if="$site.themeConfig.betaTestFormUrl">
+            <a :href="$site.themeConfig.betaTestFormUrl" target="_blank"
+              >Become a tester</a
+            >
+          </span>
+          <br />
+          <span v-if="legacyUrl" class="content-status-status">
+            <br />
+            <a target="_blank" :href="legacyUrl"
+              >View this page on legacy site</a
+            >
+          </span>
+        </p>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
