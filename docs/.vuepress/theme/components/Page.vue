f569bbc61fef3cfdef32305fccb8c1fc88bc74f3
Chris Waring <chris@wwaves.co>
2019-12-10 21:09:43 +0000

    add feedback to page template

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -4,16 +4,19 @@
       <div class="theme-default-content">
         <p>{{ $page }}</p>
       </div>
+      <Feedback />
     </template>
   </ParentLayout>
 </template>
 
 <script>
 import ParentLayout from '@parent-theme/components/Page.vue'
+import Feedback from './Feedback.vue'
 export default {
   name: 'Page',
   components: {
-    ParentLayout
+    ParentLayout,
+    Feedback
   }
 }
 </script>
