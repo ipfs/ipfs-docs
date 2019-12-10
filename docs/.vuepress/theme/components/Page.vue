aec7f4c96f2acdace670e215e054ba57bf14ed73
Chris Waring <chris@wwaves.co>
2019-12-10 17:16:47 +0000

    page inheritance

---
+++ docs/.vuepress/theme/components/Page.vue
@@ -0,0 +1,19 @@
+<template>
+  <ParentLayout>
+    <template #top>
+      <div class="theme-default-content">
+        <p>{{ $page }}</p>
+      </div>
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/components/Page.vue'
+export default {
+  name: 'Page',
+  components: {
+    ParentLayout
+  }
+}
+</script>
