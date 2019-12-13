b421311855ec50ed8eea0f8a4160bb30234daaff
Chris Waring <chris@wwaves.co>
2019-12-13 17:45:32 +0000

    add outbound link tracking

---
+++ docs/.vuepress/theme/components/Analytics.vue
@@ -0,0 +1,20 @@
+<template></template>
+
+<script>
+export default {
+  mounted: function() {
+    if (!window.ga) return
+
+    // track outbound clicks
+    document.addEventListener(
+      'click',
+      function(e) {
+        var link = e.target.closest('a')
+        if (link === null || window.location.host === link.host) return
+        ga('send', 'event', 'outbound', 'click', href)
+      },
+      false
+    )
+  }
+}
+</script>
