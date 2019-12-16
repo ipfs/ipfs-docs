e1a45fbdf0953f8bd13efe220ee629a07659924b
Chris Waring <chris@wwaves.co>
2019-12-16 13:41:09 +0000

    bind metods to mount/unmount

--- docs/.vuepress/theme/components/Analytics.vue
+++ docs/.vuepress/theme/components/Analytics.vue
@@ -3,18 +3,20 @@
 <script>
 export default {
   mounted: function() {
-    if (!window.ga) return
-
     // track outbound clicks
-    document.addEventListener(
-      'click',
-      function(e) {
-        var link = e.target.closest('a')
-        if (link === null || window.location.host === link.host) return
-        ga('send', 'event', 'outbound', 'click', href)
-      },
-      false
-    )
+    document.addEventListener('click', this.trackOutbound)
+  },
+  beforeDestroy() {
+    // remove on unmount
+    document.removeEventListener('click', this.trackOutbound)
+  },
+  methods: {
+    trackOutbound(e) {
+      if (!window.ga) return
+      var link = e.target.closest('a')
+      if (link === null || window.location.host === link.host) return
+      ga('send', 'event', 'outbound', 'click', href)
+    }
   }
 }
 </script>
