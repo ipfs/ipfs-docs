37a67f3196e1059239a70312969a1e875c386625
Jessica Schilling <jessica@protocol.ai>
2020-05-04 17:40:39 -0600

    Fixes typos, tightens language, makes styling consistent

--- docs/.vuepress/theme/components/DeprecationNotice.vue
+++ docs/.vuepress/theme/components/DeprecationNotice.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="warning custom-block">
-    <p class="custom-block-title">This site will soon be leaving beta!</p>
+  <div
+    class="warning custom-block"
+    style="margin: 2rem 2rem 0 2rem; max-width: calc(740px - 2.5rem);"
+  >
+    <p class="custom-block-title">THIS SITE LEAVES BETA MAY 15!</p>
     <p>
-      The
-      <a target="_blank" href="//docs.ipfs.io"
-        >legacy IPFS documentation website</a
-      >
-      is being deprecated on 15th May, with this site replacing it. Links to
-      this page will redirect from <code>docs-beta.ipfs.io/</code> to
-      <code>docs.ipfs.io/</code>. This redirect with happen automatically, but
-      we just thought we'd let you know.
+      On May 15, this site's content will permanently replace the
+      <a target="_blank" href="//docs.ipfs.io">old IPFS docs</a>. Pages on
+      <code>docs-beta.ipfs.io</code> will automatically redirect to
+      <code>docs.ipfs.io</code>, but you may want to update your own bookmarks
+      or links.
     </p>
   </div>
 </template>
