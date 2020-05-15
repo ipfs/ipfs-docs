5f65bc983d1709d444f9a9e767fc7156b1247d22
Johnny Matthews <johnnymatthews@protonmail.com>
2020-05-15 11:51:07 -0400

    Update deprecation date.

--- docs/.vuepress/theme/components/DeprecationNotice.vue
+++ docs/.vuepress/theme/components/DeprecationNotice.vue
@@ -3,7 +3,7 @@
     <div id="notice" class="warning custom-block">
       <p class="custom-block-title">🚀 Beta -> Launch</p>
       <p>
-        <strong>On May 15</strong>, this site's content will replace the
+        <strong>On May 27th</strong>, this site's content will replace the
         <a target="_blank" href="//docs.ipfs.io">old IPFS docs</a>. Pages on
         <code>docs-beta.ipfs.io</code> will automatically redirect to
         <code>docs.ipfs.io</code>, but you may want to update your own bookmarks
