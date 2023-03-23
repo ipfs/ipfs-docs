5a894bcb10b58ea2a8d8291a86a0d1b12cd29296
johnnymatthews <9611008+johnnymatthews@users.noreply.github.com>
2023-03-23 19:09:09 -0300

    Removes GA NPM package.

--- docs/.vuepress/head.js
+++ docs/.vuepress/head.js
@@ -3,7 +3,6 @@ module.exports = [
     'meta',
     { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
   ],
-  ['link', { rel: 'preconnect', href: 'https://www.google-analytics.com' }],
   [
     'link',
     {
