6485578c26d62209dc8eb10258a75cc979c286f2
Daniel N <2color@users.noreply.github.com>
2024-07-25 17:57:01 +0200

    feat: new ipfs on the web docs page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -253,12 +253,12 @@ module.exports = {
               ]
             },
             {
-              title: 'IPFS in the browser',
+              title: 'IPFS on the web',
               sidebarDepth: 1,
               collapsable: true,
               children: [
+                '/how-to/ipfs-on-the-web',
                 '/how-to/address-ipfs-on-web',
-                '/how-to/browser-tools-frameworks'
               ]
             },
             {
@@ -275,7 +275,7 @@ module.exports = {
               collapsable: true,
               children: [
                 '/how-to/gateway-best-practices',
-                '/how-to/gateway-troubleshooting'
+                '/how-to/gateway-troubleshooting',
               ]
             },
             {
